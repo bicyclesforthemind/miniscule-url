e145f456f292bff56631e4109cf434d0fbe65032
bicyclesforthemind <aaa2141@gmail.com>
2019-02-16 13:51:52 -0600

    init devise with JWT strategy

---
+++ miniscule-url-server/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :jwt_authenticatable,
+         :registerable, 
+         :validatable,
+         jwt_revocation_strategy: JWTBlacklist
+end
