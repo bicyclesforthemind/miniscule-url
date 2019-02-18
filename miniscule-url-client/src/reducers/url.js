5dae4ffe86eca16b0b5e15b36e1bb125901df5e9
bicyclesforthemind <aaa2141@gmail.com>
2019-02-18 09:01:26 -0600

    updated url reducer with finished functions + improved local storage support

--- miniscule-url-client/src/reducers/url.js
+++ miniscule-url-client/src/reducers/url.js
@@ -1,13 +1,62 @@
-
+import {
+    SET_URL,
+    POST_URL_REQUEST,
+    POST_URL_SUCCESS,
+    POST_URL_ERROR,
+    GET_URLS_REQUEST,
+    GET_URLS_SUCCESS,
+    GET_URLS_ERROR
+} from '../actions/url';
 
 
 const initialState = {
-    urls: []
+    urls: [],
+    url: '',
+    error: {},
+    isLoading: false
 };
 
 const urlReducer = (state = initialState, action) => {
     switch (action.type) {
-        
+        case SET_URL:
+            return {
+                ...state,
+                url: action.payload
+            };
+        case POST_URL_REQUEST: 
+            return {
+                ...state,
+                isLoading: true
+            };
+        case POST_URL_SUCCESS:
+            return {
+                ...state,
+                url: '',
+                isLoading: false
+            };
+        case POST_URL_ERROR:
+            return {
+                ...state,
+                url: '',
+                error: action.payload,
+                isLoading: false
+            };
+        case GET_URLS_REQUEST:
+            return {
+                ...state,
+                isLoading: true
+            };
+        case GET_URLS_SUCCESS:
+            return {
+                ...state,
+                urls: action.payload,
+                isLoading: false
+            };
+        case GET_URLS_ERROR:
+            return {
+                ...state,
+                isLoading: true
+            };
         default:
             return state;
     }
