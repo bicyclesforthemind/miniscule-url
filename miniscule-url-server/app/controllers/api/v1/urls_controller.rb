d30677cb0ac1956e49fd9e796fa86c34a2d072bf
bicyclesforthemind <aaa2141@gmail.com>
2019-02-19 04:06:02 -0600

    improved url model and controller based on tests

--- miniscule-url-server/app/controllers/api/v1/urls_controller.rb
+++ miniscule-url-server/app/controllers/api/v1/urls_controller.rb
@@ -10,29 +10,63 @@ module Api
             end
 
             def create 
-                # TODO: verify url
-                @url = Url.new(original_url: params[:original_url], root_url: request.base_url, hit_count: 0)
-                @url.user = current_user
-                @url.miniscule_url = generate_miniscule_url
+                if valid_url?(params[:original_url])
+                    @url = Url.new(original_url: params[:original_url], root_url: request.base_url, hit_count: 0)
+                    @url.user = current_user
+                    @url.miniscule_url = generate_miniscule_url
 
-                if @url.save
-                    render json: @url
+                    if @url.save
+                        render json: @url
+                    else
+                        render json: {
+                            errors: [
+                                status: '400',
+                                title: 'Bad Request',
+                                detail: 'Invalid URL'
+                            ]
+                        }, status: :bad_request
+                    end
                 else
-                    render json: {}.to_json #TODO: add error
+                    render json: {
+                        errors: [
+                            status: '400',
+                            title: 'Bad Request',
+                            detail: 'Invalid URL'
+                        ]
+                    }, status: :bad_request
                 end
             end
 
             def show
+                # navigate to new url and increment hit count.
                 @url = Url.find_by(miniscule_url: params[:miniscule_url])
-                @url.hit_count += 1
-                @url.save
-                redirect_to @url.original_url
+                if @url
+                    @url.hit_count += 1
+                    @url.save
+                    redirect_to @url.original_url
+                else 
+                    render json: {
+                        errors: [
+                            status: '404',
+                            title: 'Not Found',
+                            detail: 'Invalid URL'
+                        ]
+                    }, status: :not_found
+                end
             end
 
             private
 
             def generate_miniscule_url
                 SecureRandom.urlsafe_base64(6)
+            end
+
+            def valid_url?(url)
+                # check if url is valid or not
+                uri = URI.parse(url)
+                (uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)) && !uri.host.nil?
+            rescue URI::InvalidURIError
+                false
             end
         end
     end
--- miniscule-url-server/app/models/url.rb
+++ miniscule-url-server/app/models/url.rb
@@ -1,3 +1,4 @@
 class Url < ApplicationRecord
   belongs_to :user
+  default_scope { order(hit_count: :desc) }
 end
